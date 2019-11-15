68c903cda536ad15065b334c660ec3ed421d9ddd
hasnainalimj <mjhasnain15@gmail.com>
2019-11-15 23:53:11 +0500

    React Class 2

--- class_two/src/App.js
+++ class_two/src/App.js
@@ -1,36 +1,81 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  caller() {
+    alert("hellow");
+  }
+
+  callMe = () => {
+    this.caller();
+  };
+
+  getObj(obj) {
+    console.log(obj);
+  }
+
   render() {
+    // this.caller();
+    // this.callMe();
+    console.log("render");
+
     const classDate = "First React Class";
-    const list = [
-      {
-        title: "React",
-        url: "https://facebook.github.io/react/",
-        author: "Jordan Walke",
-        num_comments: 3,
-        points: 4,
-        objectID: 0
-      },
-      {
-        title: "Redux",
-        url: "https://github.com/reactjs/redux",
-        author: "Dan Abramov, Andrew Clark",
-        num_comments: 2,
-        points: 5,
-        objectID: 1
-      }
+    const list = ["abc", [1, 2, [1, 3, 4, [9, 8, 7], 5], 3], [4, 5, 6]];
+
+    var users = [
+      { id: 1, name: "ali" },
+      { id: 2, name: "saad" },
+      { id: 3, name: "khan" }
     ];
+
     return (
       <div className="App">
         <h1>React Class One..........</h1>
         <h1>{classDate}</h1>
-        {list.map((value, index) => {
+        {/* {list.map((value, index) => {
+          return value.map(val => {
+            return <h1>{val}</h1>;
+          });
+        })} */}
+
+        {/* {list.map(val => {
+          console.log(val);
+          if (typeof val == "number" || typeof val == "string") {
+            return <h1>{val}</h1>;
+          }
+
+          if (typeof val == "object") {
+            return val.map(val2 => {
+              if (typeof val2 == "number" || typeof val2 == "string") {
+                return <h1>{val2}</h1>;
+              }
+
+              if (typeof val2 == "object") {
+                return val2.map(val3 => {
+                  if (typeof val3 == "number" || typeof val3 == "string") {
+                    return <h1>{val3}</h1>;
+                  }
+
+                  if (typeof val3 == "object") {
+                    return val3.map(val4 => {
+                      return <h1>{val4}</h1>;
+                    });
+                  }
+                });
+              }
+            });
+          }
+        })} */}
+
+        {users.map(val => {
           return (
-            <div key={index}>
-              <h1>{value.title}</h1>
-              <p>{value.points}</p>
+            <div>
+              <h1 onClick={this.getObj.bind(this, val)}>{val.id}</h1>
+              <h1>{val.name}</h1>
             </div>
           );
         })}
